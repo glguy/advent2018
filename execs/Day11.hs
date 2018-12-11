ecb3628c4b5c1f6299b06ff4b3f1dbe3f81bf636
Eric Mertens <emertens@gmail.com>
2018-12-11 08:25:29 -0800

    Use a summed area table
    This improves on my previous dynamic programming solution by not having to recompute the side sums

--- execs/Day11.hs
+++ execs/Day11.hs
@@ -15,50 +15,72 @@ import           Data.Ord      (comparing)
 import           Data.Foldable (maximumBy)
 import qualified Data.Array.Unboxed as A
 
-type Grid = A.Array (Int,Int) Int
+type Grid = A.UArray (Int,Int) Int
+
+dim :: Int
+dim = 300
 
 -- | Print the answers to day 11
 main :: IO ()
 main =
   do [serial] <- getParsedLines 11 number
      let cells = mkCells serial
-     print (part1 cells)
-     print (part2 cells)
+     let areas = summedArea cells
+     print (part1 areas)
+     print (part2 areas)
 
 part1 :: Grid -> (Int, Int)
-part1 cells = best $ A.assocs $ growSquares cells $ growSquares cells cells
+part1 areas = best (squaresOfSize areas 3)
 
 part2 :: Grid -> (Int, Int, Int)
-part2 cells = best [ ((x,y,n),e)
-                   | (n,vs) <- zip [1..300] (iterate (growSquares cells) cells)
-                   , ((x,y),e) <- A.assocs vs ]
+part2 areas = best [ ((x,y,sz),e) | sz <- [1..dim], ((x,y),e) <- squaresOfSize areas sz]
 
 best :: Ord b => [(a,b)] -> a
 best = fst . maximumBy (comparing snd)
 
-powerLevel :: Int -> Int -> Int -> Int
-powerLevel serial x y = (rackid * y + serial) * rackid `div` 100 `mod` 10 - 5
+powerLevel :: Int -> (Int, Int) -> Int
+powerLevel serial (x, y) = (rackid * y + serial) * rackid `div` 100 `mod` 10 - 5
   where rackid = x+10
 
 mkCells :: Int -> Grid
-mkCells serial =
-  A.array ((1,1),(300,300))
-  [ ((x,y), powerLevel serial x y) | x <- [1..300], y <- [1..300] ]
+mkCells serial = generate (1,1) (dim,dim) (powerLevel serial)
 
-growSquares :: Grid -> Grid -> Grid
-growSquares cells prev =
-  A.array ((1,1),(n',n'))
-  [ ((x,y), prev  A.! (x+1,y+1)
-          + cells A.! (x,y)
-          + sum [ cells A.! (x,y+z)
-                + cells A.! (x+z,y)
-                | z <- [1 .. h]
-                ]
-    )
-  | x <- [1..n']
-  , y <- [1..n'] ]
+squaresOfSize :: Grid -> Int -> [((Int,Int),Int)]
+squaresOfSize areas sz =
+  [ ((x,y), squareSum areas x y sz)
+  | x <- [1..dim-sz+1]
+  , y <- [1..dim-sz+1]
+  ]
 
+squareSum :: Grid -> Int -> Int -> Int -> Int
+squareSum areas x y sz
+  = areas A.! (x+sz-1,y+sz-1)
+  - areas A.! (x   -1,y+sz-1)
+  - areas A.! (x+sz-1,y   -1)
+  + areas A.! (x   -1,y   -1)
+
+summedArea :: Grid -> Grid
+summedArea elts = convert table -- convert to unboxed array
   where
-    gridSize = fst . snd . A.bounds
-    n' = gridSize prev  - 1
-    h =  gridSize cells - gridSize prev + 1
+  -- table is boxed to allow self-reference in definition
+  table = A.array ((0,0),(dim,dim))
+        $ concat [ [ ((0,z),0), ((z,0),0)] | z <- [0..dim]]
+        ++
+        [ ( (x,y)
+          , elts  A.! (x,y)
+          + table A.! (x-1,y)
+          + table A.! (x,y-1)
+          - table A.! (x-1,y-1)
+          )
+        | x <- [1..dim]
+        , y <- [1..dim]
+        ]
+
+-- Array helpers
+
+generate :: (A.IArray a e, A.Ix i) => i -> i -> (i -> e) -> a i e
+generate lo hi f = A.array (lo,hi) [ (i, f i) | i <- A.range (lo, hi) ]
+
+convert :: (A.IArray a e, A.Ix i) => A.Array i e -> a i e
+convert a = A.array (A.bounds a) (A.assocs a)
+
