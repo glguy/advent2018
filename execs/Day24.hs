34d8759ebeebf067cfdc8e975ada7221a3621397
Eric Mertens <emertens@galois.com>
2018-12-28 17:51:31 -0600

    Scrub #24

--- execs/Day24.hs
+++ execs/Day24.hs
@@ -47,12 +47,13 @@ data Element = Bludgeoning | Fire | Slashing | Radiation | Cold
 main :: IO ()
 main =
   do groups <- getParsedInput 24 parseInput
-     print (evaluate (simulate groups))
-     print (evaluate (search (attemptBoost groups) 1 Nothing))
+     let groupMap = IntMap.fromList [ (initiative g, g) | g <- groups ]
+     print (evaluate (simulate groupMap))
+     print (evaluate (search (attemptBoost groupMap) 1 Nothing))
 
 -- | Find the sum of the sizes of all the groups.
-evaluate :: [Group] -> Int
-evaluate = sum . map size
+evaluate :: IntMap Group -> Int
+evaluate = sum . fmap size
 
 -- | Determine the lowest value that satisfies the given
 -- predicate and return the result.
@@ -77,7 +78,7 @@ isGood g = Good == team g
 
 -- | Determine if a boost is enough to allow the reindeer to
 -- win. If it is return the final group of reindeer.
-attemptBoost :: [Group] -> Int -> Maybe [Group]
+attemptBoost :: IntMap Group -> Int -> Maybe (IntMap Group)
 attemptBoost groups boost
   | all isGood outcome = Just outcome
   | otherwise          = Nothing
@@ -86,20 +87,25 @@ attemptBoost groups boost
       | isGood g  = g { attack = attack g + boost }
       | otherwise = g
 
-    outcome = simulate (map boostGood groups)
+    outcome = simulate (fmap boostGood groups)
+
+-- battle logic --------------------------------------------------------
 
 -- | Run a battle until it stops making progress due to immunities
 -- or due to a team being wiped out.
-simulate :: [Group] -> [Group]
+simulate :: IntMap Group -> IntMap Group
 simulate groups
   | groups == groups' = groups
-  | otherwise = simulate groups'
+  | otherwise         = simulate groups'
   where
     groups' = combat groups
 
 -- | Determine the effectiveness multiplier of an attack element against
 -- a particular group.
-effectiveness :: Group -> Group -> Int
+effectiveness ::
+  Group {- ^ attacker          -} ->
+  Group {- ^ defender          -} ->
+  Int   {- ^ damage multiplier -}
 effectiveness atk def =
   case lookup (attackElement atk) (special def) of
     Just Immune -> 0
@@ -112,58 +118,85 @@ effectivePower grp = size grp * attack grp
 
 -- | Order a list of groups by the order they get to chose their
 -- targets.
-targetSelectionOrder :: [Group] -> [Group]
+targetSelectionOrder ::
+  [Group] {- ^ unordered groups        -} ->
+  [Group] {- ^ target preference order -}
 targetSelectionOrder groups = sortOn prj groups
   where
+    -- ascending sort lexicographic order:
+    -- * effectiveness (negated for descending order)
+    -- * initiative    (negated for descending order)
     prj grp = (negate (effectivePower grp), negate (initiative grp))
 
 -- | Given a list of groups generate a targetting assignment.
 targetSelection ::
-  [Group]      {- ^ unordered groups        -} ->
-  [(Int, Int)] {- ^ attacker / defender IDs -}
+  [Group]    {- ^ unordered groups        -} ->
+  IntMap Int {- ^ attacker / defender IDs -}
 targetSelection groups =
-  foldr aux (\_ -> []) (targetSelectionOrder groups) groups
-  where
-  aux atk next groups =
-    case targetChoice atk groups of
-      Nothing -> next groups
-      Just def -> (initiative atk, initiative def)
-                : next (delete def groups)
+  fst (foldl' chooseTarget (IntMap.empty, groups) (targetSelectionOrder groups))
+
+-- | Given the current targetting assignment and a list of the groups
+-- not yet targeted, update the assignment and list given the
+-- preferences of the next attacker.
+chooseTarget ::
+  (IntMap Int, [Group]) {- ^ targets so far and remaining groups -} ->
+  Group                 {- ^ attacker                            -} ->
+  (IntMap Int, [Group])
+chooseTarget (targets, groups) atk =
+  fromMaybe (targets, groups) $ -- use previous values if no target
+  do def <- targetChoice atk groups
+     let targets' = IntMap.insert (initiative atk) (initiative def) targets
+         groups'  = delete def groups
+     targets' `seq` Just (targets', groups')
 
 -- | Given a group and the list of remaining target choices, determine
 -- the chosen group, if any.
-targetChoice :: Group -> [Group] -> Maybe Group
-targetChoice atk def
-  | null def' = Nothing
-  | otherwise = Just $! choice
+targetChoice ::
+  Group   {- ^ attacker          -} ->
+  [Group] {- ^ elligible targets -} ->
+  Maybe Group
+targetChoice atk groups = maximumOn prj (filter (isValidTarget atk) groups)
   where
+    -- maximum lexicographic order:
+    -- * effectiveness   (prefer targeting groups weak to this unit)
+    -- * effective power (prefer targeting strong groups)
+    -- * initiative      (prefer targeting high initiative groups)
     prj def = (effectiveness atk def, effectivePower def, initiative def)
-    choice = maximumBy (comparing prj) def'
-    def' = filter (\g -> effectiveness atk g > 0
-                      && team g /= team atk) def
+
+-- | Check if one unit can attack another.
+isValidTarget ::
+  Group {- ^ attacker            -} ->
+  Group {- ^ defender            -} ->
+  Bool  {- ^ valid attack choice -}
+isValidTarget atk def = team atk /= team def      -- no friendly fire
+                     && effectiveness atk def > 0 -- skip if immune
 
 -- | Given an unordered list of groups, compute the result of combat
-combat :: [Group] -> [Group]
-combat groups =
-  foldr aux IntMap.elems combatOrder (toIntMap groups)
+combat :: IntMap Group -> IntMap Group
+combat groups = foldl' (combatTurn targets) groups combatOrder
   where
-    selection = targetSelection groups
+    targets     = targetSelection (IntMap.elems groups)
 
-    combatOrder = sortBy (flip compare) (map initiative groups)
+    combatOrder = reverse (IntMap.keys groups)
 
-    toIntMap xs = IntMap.fromList [ (initiative g, g) | g <- xs ]
-
-    aux atkid next groups =
-      fromMaybe (next groups) $
-        do defid <- lookup atkid selection
-           atk <- IntMap.lookup atkid groups
-           def <- IntMap.lookup defid groups
-           let dmg = effectiveness atk def * effectivePower atk
-               killed = min (size def) (dmg `quot` hp def)
-               size' = size def - killed
-           if size' > 0
-               then return (next (IntMap.insert defid def { size = size' } groups))
-               else return (next (IntMap.delete defid groups))
+-- Apply the combat effect for a specific group identified by its ID
+combatTurn ::
+  IntMap Int   {- ^ targets        -} ->
+  IntMap Group {- ^ all groups     -} ->
+  Int          {- ^ attacker ID    -} ->
+  IntMap Group {- ^ updated groups -}
+combatTurn targets groups atkid =
+  fromMaybe groups $ -- in case of no target or dead group, no change
+  do defid <- IntMap.lookup atkid targets
+     atk   <- IntMap.lookup atkid groups
+     def   <- IntMap.lookup defid groups
+     let dmg    = effectiveness atk def * effectivePower atk
+         killed = min (size def) (dmg `quot` hp def)
+         size'  = size def - killed
+         groups'
+           | size' > 0 = IntMap.insert defid def { size = size' } groups
+           | otherwise = IntMap.delete defid groups
+     Just groups'
 
 -- input parsing -------------------------------------------------------
 
@@ -204,3 +237,13 @@ parseElement =
 
 parseEffect :: Parser Effect
 parseEffect = Weak <$ "weak" <|> Immune <$ "immune"
+
+-- foldable utility function -------------------------------------------
+
+-- | Returns the element that maximizes the result of a key function
+-- applied to the elements. See also: 'sortOn'
+maximumOn :: (Foldable t, Ord b) => (a -> b) -> t a -> Maybe a
+maximumOn prj xs
+  | null xs   = Nothing
+  | otherwise = Just $! maximumBy (comparing prj) xs
+
